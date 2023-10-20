547bc81155130ac841a9751f07a87004c9155599
g002alghisi <g.alghisi002@gmail.com>
2023-10-20 20:43:50 +0200

    Trying to use the bridge. Slightly modified run_hostapd.sh to turn off also eth connection.

--- Src/run_hostapd.sh
+++ Src/run_hostapd.sh
@@ -24,18 +24,35 @@ config_file="../Config/hostapd.conf"
 setup() {
     stty -echo  # Hide ^C
 
-    current_connection="$(nmcli c show --active | grep wifi | cut -d ' ' -f 1)"
+    current_eth_connection="$(nmcli -t c show --active | grep ethernet | cut -d ':' -f 1)"
 
-    if [ -z $current_connection ]; then
+    if [ -z "$current_eth_connection" ]; then
+        echo "Currently not connected to any Ethernet." 
+    else
+        echo "Disconnecting from $current_eth_connection [Eth]... "
+        nmcli c down "$current_eth_connection"   
+    fi
+    echo ""
+
+
+    current_wifi_connection="$(nmcli -t c show --active | grep wifi | cut -d ':' -f 1)"
+
+    if [ -z "$current_wifi_connection" ]; then
         echo "Currently not connected to any WiFi." 
     else
-        echo "Disconnecting from $current_connection... "
-        nmcli c down $current_connection   
+        echo "Disconnecting from $current_wifi_connection [WiFi]..."
+        nmcli c down "$current_wifi_connection"   
     fi
-
     echo ""
+
+}
+
+
+launch_hostapd() {
     echo -e "${YELLOW}Hostapd is up. Press Ctrl-C to terminate...${NC}"
     echo ""
+
+    sudo hostapd "$config_file"
 }
 
 
@@ -43,7 +60,9 @@ setdown() {
     echo "" # New line
     echo -e "${PURPLE}Hostapd is down.${NC}"
     echo ""
+
     stty echo   # Show ^C
+    
     exit 0
 }
 
@@ -52,7 +71,7 @@ setdown() {
 ### Main section of the program
 setup
 
-sudo hostapd "$config_file"
+launch_hostapd
 
 setdown
 
