bafac38fa2b778eb5fd27a8112fc2df216a3cda7
g002alghisi <g.alghisi002@gmail.com>
2023-11-09 22:09:42 +0100

    Nothing new.

--- ESP32/wifi_scanner/wifi_scanner.ino
+++ ESP32/wifi_scanner/wifi_scanner.ino
@@ -8,33 +8,31 @@
 void setup()
 {
     Serial.begin(115200);
-    while(!Serial){
-      delay(100);
-    }
 
     // Set WiFi to station mode and disconnect from an AP if it was previously connected
     WiFi.mode(WIFI_STA);
     WiFi.disconnect();
     delay(100);
 
-    Serial.println("Setup done.");
+    Serial.println("Setup done");
 }
+
 
 void loop()
 {
-    Serial.println("Starting scan...");
+    Serial.println("scan start");
 
     // WiFi.scanNetworks will return the number of networks found
     int n = WiFi.scanNetworks();
-    Serial.println("Done.");
+    Serial.println("scan done");
     if (n == 0) {
-        Serial.println("No networks found.");
+        Serial.println("no networks found");
     } else {
         Serial.print(n);
-        Serial.println(" networks found.");
-        for (int i = 1; i <= n; ++i) {
+        Serial.println(" networks found");
+        for (int i = 0; i < n; ++i) {
             // Print SSID and RSSI for each network found
-            Serial.print(i);
+            Serial.print(i + 1);
             Serial.print(": ");
             Serial.print(WiFi.SSID(i));
             Serial.print(" (");
